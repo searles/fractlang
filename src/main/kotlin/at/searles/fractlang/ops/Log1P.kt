50eec019602e0657e00aee512bc43b5d6495efa3
searles <karl.gmeiner@gmail.com>
2020-03-13 08:29:52 +0100

    Add arity check to newton and simplify log1p

--- src/main/kotlin/at/searles/fractlang/ops/Log1P.kt
+++ src/main/kotlin/at/searles/fractlang/ops/Log1P.kt
@@ -1,26 +1,24 @@
 package at.searles.fractlang.ops
 
-import at.searles.fractlang.BaseTypes
 import at.searles.fractlang.nodes.*
+import at.searles.fractlang.semanticanalysis.SemanticAnalysisException
 import at.searles.parsing.Trace
-import kotlin.math.ln
 
 /**
  * log(x + 1)
  */
-object Log1P: BaseOp (
-    Signature(BaseTypes.Real, BaseTypes.Real)
-) {
+object Log1P: Op {
 
-    override fun evaluate(trace: Trace, args: List<Node>): Node {
-        return when {
-            args[0] is RealNode -> RealNode(trace, ln((args[0] as RealNode).value + 1))
-            else -> Log.createApp(trace,
-                Add.createApp(trace,
-                    RealNode(trace, 1.0),
-                    args[0]
-                )
-            )
+    override fun apply(trace: Trace, args: List<Node>): Node {
+        if(args.size != 1) {
+            throw SemanticAnalysisException("'log1p' requires one argument: log1p(x)", trace)
         }
+
+        return Log.apply(trace,
+            Add.apply(trace,
+                RealNode(trace, 1.0),
+                args[0]
+            )
+        )
     }
 }
--- src/main/kotlin/at/searles/fractlang/ops/Newton.kt
+++ src/main/kotlin/at/searles/fractlang/ops/Newton.kt
@@ -1,10 +1,15 @@
 package at.searles.fractlang.ops
 
 import at.searles.fractlang.nodes.Node
+import at.searles.fractlang.semanticanalysis.SemanticAnalysisException
 import at.searles.parsing.Trace
 
 object Newton: Op {
     override fun apply(trace: Trace, args: List<Node>): Node {
+        if(args.size != 2) {
+            throw SemanticAnalysisException("'newton' requires two arguments: newton(f, var)", trace)
+        }
+
         return Sub.apply(trace,
             args[1],
             Div.apply(trace,
